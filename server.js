cadb28cac906e7f9ece99f9b1c418075438e9428
CodePokeX <sonta445@gmail.com>
2025-02-07 17:30:40 +0530

    Sign In page created Commit

--- server.js
+++ server.js
@@ -1,6 +1,7 @@
 const express = require('express');
 const path = require('path');
 require('dotenv').config(); // Load environment variables
+const bodyParser = require('body-parser'); // Middleware to parse form data
 
 const app = express();
 const PORT = process.env.PORT || 3500;
@@ -8,9 +9,15 @@ const PORT = process.env.PORT || 3500;
 // Set EJS as the templating engine
 app.set('view engine', 'ejs');
 
+// Middleware for parsing form data
+app.use(bodyParser.urlencoded({ extended: true }));
+
 app.use('/', express.static(path.join(__dirname, '/static')));
 
 app.use('/', require('./routes/root.js'));
+
+// Use the sign_in route
+app.use('/sign-in', require('./routes/sign_in'));
 
 app.all('*', (req, res)=>{
     res.status(404);
